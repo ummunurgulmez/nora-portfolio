4eb73c4f453246541c18762b53c1a962a2fd1fc3
Ümmü Nur Gülmez <ummunur.gulmez@gazi.edu.tr>
2025-01-07 00:41:38 +0300

    Projects images changed.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,10 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg1 from "../assets/img/Development of a Secure Communication Protocol for UAV.jpg";
+import projImg2 from "../assets/img/Cryptocurrency Forecasting.jpg";
+import projImg3 from "../assets/img/Data Journalism with Anime.jpg";
+import projImg4 from "../assets/img/Bridge Web Site for disabled people.jpg";
+import projImg5 from "../assets/img/Shopping App - Pochette.jpg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -11,35 +13,35 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Development of a Secure Communication Protocol for UAVs",
+      description: "Python, Encryption Algorithms(ECC/AES-GCM), NS-3 Simulation, OpenSSL, SHA-256, E-signature",
       imgUrl: projImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Cryptocurrency Forecasting",
+      description: "Python, Machine Learning Model (LSTM), Data Manipulation Libraries (Pandas, NumPy), Data Visualization (Matplotlib, Seaborn), Evaluation Metrics (MSE, RMSE)",
       imgUrl: projImg2,
+      link: "https://github.com/kutlinho/Team9_CryptocurrencyForecasting"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Data Journalism with Anime",
+      description: "Python, Data Manipulation Libraries (Pandas, NumPy), Data Visualization (Matplotlib, Seaborn)",
       imgUrl: projImg3,
+      link: "https://github.com/ummunurgulmez/Data-Journalism-with-Anime"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Shopping App - Pochette",
+      description: "Java, Firebase Database, Android Studio, OOP Principles",
+      imgUrl: projImg5,
+      link: "https://github.com/ummunurgulmez/android-java-alisveris-uygulamasi"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Bridge Web Site",
+      description: "Flutter, Python, Django, HTML/CSS",
+      imgUrl: projImg4,
+      link: "https://github.com/ummunurgulmez/bridge_web"
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+
   ];
 
   return (
